11d67fba1f50c1fd6cbd58493f7f0efd60af38c2
Jaxon Tabesh <78238761+JaxonTabesh@users.noreply.github.com>
2025-10-14 00:09:29 -0700

    fix: escape the apostrophe that is ruining compilation

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,13 +1,11 @@
-import { FaGithub, FaLinkedin, FaEnvelope, FaFileAlt } from 'react-icons/fa';
-import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
 import SocialIconComponent, { SocialIcon } from '@/components/social-icon';
 
 export default function Hero() {
   return (
     <section id="hero">
       <div className="flex h-screen flex-col items-center justify-center text-center dark:text-gray-400">
-        <h2 className="m-2 text-center text-9xl">
-          <span>Hi, I'm </span>
+        <h2 className="m-8 text-center text-9xl">
+          <span>{"Hi, I'm "}</span>
           <span className="text-[#e03131]">Jaxon Tabesh</span>.
         </h2>
         <div className="flex items-center gap-6 p-2 text-8xl">
@@ -16,23 +14,6 @@ export default function Hero() {
           <SocialIconComponent type={SocialIcon.Email} />
           <SocialIconComponent type={SocialIcon.Resume} />
         </div>
-        <button>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-28"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-              className="fill-gray-500 p-0 transition-colors hover:fill-[#e03131]"
-            />
-          </svg>
-        </button>
       </div>
     </section>
   );
