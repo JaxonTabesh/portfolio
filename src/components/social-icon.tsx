bd9366cd160933e53c0e8fa9052936dfa1897fa9
Jaxon Tabesh <78238761+JaxonTabesh@users.noreply.github.com>
2025-10-13 18:33:09 -0700

    feat: add SocialIcon component for displaying social links with tooltips

---
+++ src/components/social-icon.tsx
@@ -0,0 +1,58 @@
+import { Tooltip, TooltipTrigger, TooltipContent } from '@/components/ui/tooltip';
+import { FaGithub, FaLinkedin, FaEnvelope, FaFileAlt } from 'react-icons/fa';
+import type { IconType } from 'react-icons';
+
+export enum SocialIcon {
+  GitHub = 'GitHub',
+  LinkedIn = 'LinkedIn',
+  Email = 'Email',
+  Resume = 'Résumé',
+}
+
+interface SocialIconProps {
+  type: SocialIcon;
+}
+
+export default function SocialIconComponent({ type }: SocialIconProps) {
+  let href: string;
+  let Icon: IconType;
+  let tooltip: string;
+
+  switch (type) {
+    case SocialIcon.GitHub:
+      href = 'https://github.com/jaxontabesh';
+      Icon = FaGithub;
+      tooltip = 'GitHub';
+      break;
+    case SocialIcon.LinkedIn:
+      href = 'https://www.linkedin.com/in/jaxon-tabesh-08b2642b5/';
+      Icon = FaLinkedin;
+      tooltip = 'LinkedIn';
+      break;
+    case SocialIcon.Email:
+      href = 'mailto:jaxon@jaxontabesh.com';
+      Icon = FaEnvelope;
+      tooltip = 'jaxon@jaxontabesh.com';
+      break;
+    case SocialIcon.Resume:
+      href = '/Resume.pdf';
+      Icon = FaFileAlt;
+      tooltip = 'Résumé';
+      break;
+    default:
+      throw new Error(`Unhandled icon type: ${type}`);
+  }
+
+  return (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <a href={href} target="_blank" rel="noreferrer">
+          <Icon className="transition-colors hover:fill-[#e03131]" />
+        </a>
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>{tooltip}</p>
+      </TooltipContent>
+    </Tooltip>
+  );
+}
