4fa2dc684899063da0276d71687c20da2412efcd
Jaxon Tabesh <78238761+JaxonTabesh@users.noreply.github.com>
2025-10-13 18:13:00 -0700

    feat: implement Hero component for homepage with social links and animations

---
+++ src/components/hero.tsx
@@ -0,0 +1,53 @@
+import { FaGithub, FaLinkedin, FaEnvelope, FaFileAlt } from 'react-icons/fa';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
+
+export default function Hero() {
+  return (
+    <section id="hero">
+      <div className="flex h-screen flex-col items-center justify-center text-center">
+        <h2 className="m-2 text-center text-9xl">
+          <span>Hi, I'm </span>
+          <span className="text-[#e03131]">Jaxon Tabesh</span>.
+        </h2>
+        <div className="flex items-center gap-6 p-2 text-8xl">
+          <Tooltip>
+            <TooltipTrigger>
+              <a href="https://github.com/jaxontabesh" target="_blank">
+                <FaGithub className="transition-colors hover:fill-[#e03131]" />
+              </a>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Github</p>
+            </TooltipContent>
+          </Tooltip>
+          <a href="https://www.linkedin.com/in/jaxon-tabesh-08b2642b5/" target="_blank">
+            <FaLinkedin className="transition-colors hover:fill-[#e03131]" />
+          </a>
+          <a href="mailto:jaxon@jaxontabesh.com" target="_blank">
+            <FaEnvelope className="transition-colors hover:fill-[#e03131]" />
+          </a>
+          <a href="/Resume.pdf" target="_blank">
+            <FaFileAlt className="transition-colors hover:fill-[#e03131]" />
+          </a>
+        </div>
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-28"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              className="fill-gray-500 p-0 transition-colors hover:fill-[#e03131]"
+            />
+          </svg>
+        </button>
+      </div>
+    </section>
+  );
+}
