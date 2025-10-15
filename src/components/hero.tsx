c095a79c8eca695a1cb129782878471f58114459
Jaxon Tabesh <78238761+JaxonTabesh@users.noreply.github.com>
2025-10-15 14:57:54 -0700

    fix: adjust font sizes for hero section to improve responsiveness

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,11 +4,11 @@ export default function Hero() {
   return (
     <section id="hero">
       <div className="flex h-screen flex-col items-center justify-center text-center dark:text-gray-400">
-        <h2 className="m-8 text-center text-9xl">
+        <h2 className="m-8 text-center text-6xl md:text-9xl">
           <span>{"Hi, I'm "}</span>
           <span className="text-[#e03131]">Jaxon Tabesh</span>.
         </h2>
-        <div className="flex items-center gap-6 p-2 text-8xl">
+        <div className="flex items-center gap-6 p-2 text-6xl md:text-8xl">
           <SocialIconComponent type={SocialIcon.GitHub} />
           <SocialIconComponent type={SocialIcon.LinkedIn} />
           <SocialIconComponent type={SocialIcon.Email} />
